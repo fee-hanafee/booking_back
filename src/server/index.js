2e9d739b9d2bcf95bc0da1a203fbee2b9d3f1e38
fee-hanafee <hanafee.pass@gmail.com>
2024-03-22 12:15:12 +0700

    setup middleware

--- src/server/index.js
+++ src/server/index.js
@@ -1,26 +1,30 @@
-const express = require('express')
-const { json, urlencoded } = require("express")
-const cors = require("cors")
-const morgan = require("morgan")
+const express = require("express");
+const { json, urlencoded } = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+
+const { errorMiddlewares } = require("../middlewares/error");
+const { notFound } = require("../middlewares/notFound");
 
 module.exports = function restApiServer(app) {
-    app.use(morgan('dev'))
-    app.use(cors())
-    app.use(json())
-    app.use(urlencoded({ extended: false }))
-    app.use(express.static("public"))
-    app.use("/ping", (req, res, next) => {
-        try {
-            console.log("Checking the API status: Everything is OK")
-            res.status(200).json("ping")
-        } catch (error) {
-            next(new CustomError("Ping Error", "NotFoundData", 500))
-        }
-    })
+  app.use(morgan("dev"));
+  app.use(cors());
+  app.use(json());
+  app.use(urlencoded({ extended: false }));
+  app.use(express.static("public"));
+  app.use("/ping", (req, res, next) => {
+    try {
+      console.log("Checking the API status: Everything is OK");
+      res.status(200).json("ping");
+    } catch (error) {
+      next(new CustomError("Ping Error", "NotFoundData", 500));
+    }
+  });
 
+  app.get("/", (req, res) => {
+    res.send("<h1>Hello</h1>");
+  });
 
-    app.get('/',(req,res)=> {
-        res.send("<h1>Hello</h1>")
-    })
-    
-}
+  app.use(errorMiddlewares);
+  app.use(notFound);
+};
---
+++ src/utils/index.js
@@ -0,0 +1,8 @@
+const utils = {
+  jwt: require("./jwt"),
+  bcrypt: require("./bcrypt"),
+  catchError: require("./catch-error"),
+  cloudinaryUpload: require("./cloudinaryUpload"),
+};
+
+module.exports = utils;
